a067111ee5225a1e641537f107247db2fa0256ca
Efrain Lemus <efraintlemusm@gmail.com>
2023-01-26 13:26:00 -0700

    add styling and yacht section

--- themes/insideoutcreative/partials/content.php
+++ themes/insideoutcreative/partials/content.php
@@ -223,10 +223,82 @@ endwhile; endif;
 
     echo '<section class="pt-5 pb-5 ' . get_sub_field('classes') . '" style="' . get_sub_field('style') . '" id="' . get_sub_field('id') . '">';
 
+    if(get_sub_field('content')){
+        echo '<div class="container">';
+        echo '<div class="row justify-content-center">';
+        echo '<div class="col-lg-9">';
+
+        echo get_sub_field('content');
+
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+    }
+
     echo wp_get_attachment_image($img['id'],'full','',['class'=>'w-100 h-auto']);
 
     echo '</section>';
     
+    endwhile; endif;
+} elseif ($layout == 'Yachts'){
+    if(have_rows('yachts_group')): while(have_rows('yachts_group')): the_row();
+    $bgImg = get_sub_field('background_image');
+    echo '<section class="pt-5 pb-5 position-relative ' . get_sub_field('classes') . '" style="' . get_sub_field('style') . '" id="' . get_sub_field('id') . '">';
+
+    if($bgImg){
+        echo wp_get_attachment_image($bgImg['id'],'full','',['class'=>'w-100 h-100 position-absolute','style'=>'top:0;left:0;object-fit:cover;']);
+    }
+
+    if(get_sub_field('content')){
+        echo '<div class="container">';
+        echo '<div class="row justify-content-center">';
+        echo '<div class="col-lg-9 text-center">';
+
+        echo get_sub_field('content');
+
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+    }
+
+    $yachts = get_sub_field('yachts');
+
+    if( $yachts ):
+        echo '<div class="container">';
+        echo '<div class="row justify-content-center">';
+    foreach( $yachts as $post ): 
+    // Setup this post for WP functions (variable must be named $post).
+    setup_postdata($post);
+        echo '<a href="' . get_the_permalink() . '" class="col-lg-4 col-md-6 text-center mb-5 text-white" style="">';
+        echo '<div class="img-hover overflow-h">';
+            the_post_thumbnail('full',array(
+                'class'=>'w-100',
+                'style'=>'height:250px;object-fit:cover;'
+            ));
+        echo '</div>';
+        echo '<h3 class="text-white cormorant-garamond h5 text-uppercase pt-3 ls-2">' . get_the_title() . '</h3>';
+        echo '<span class="text-accent-tertiary text-uppercase ls-2" style="">LEARN MORE</span>';
+        echo '</a>';
+    endforeach;
+        // Reset the global post object so that the rest of the page works correctly.
+        wp_reset_postdata(); 
+        echo '</div>';
+        echo '</div>';
+    endif;
+
+    if(get_sub_field('content_bottom')){
+        echo '<div class="container">';
+        echo '<div class="row justify-content-center">';
+        echo '<div class="col-lg-9 text-center">';
+
+        echo get_sub_field('content_bottom');
+
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+    }
+
+    echo '</section>';
     endwhile; endif;
 }
 endwhile; endif;
